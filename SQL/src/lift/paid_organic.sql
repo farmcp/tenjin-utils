4e1a3c6ea6571d43b6f33504d057afae915062a5
Christopher Farm <farm.cp@gmail.com>
2020-01-30 18:17:21 -0800

    make change to just date

--- SQL/src/lift/paid_organic.sql
+++ SQL/src/lift/paid_organic.sql
@@ -1,4 +1,4 @@
-SELECT date_trunc('day', date) ,
+SELECT date,
   sum(case when ad_networks.id = 0 then tracked_installs else null end) as organic_installs,
   sum(case when ad_networks.id <> 0 then tracked_installs else null end) as paid_installs
 FROM tenjin.reporting_metrics
@@ -6,7 +6,7 @@ inner join campaigns
   ON campaigns.id = reporting_metrics.campaign_id
 inner join ad_networks
   ON campaigns.ad_network_id = ad_networks.id
-where date >= '@START_DATE' and date <= '@END_DATE' 
+where date >= '@START_DATE' and date <= '@END_DATE'
   and reporting_metrics.app_id = '@APP_ID'
 GROUP BY 1
 ORDER BY 1 DESC;
